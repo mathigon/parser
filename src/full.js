af8bd41f01e0fe2466c489a3759da110a18e1cc1
Philipp Legner <philipp@mathigon.org>
2017-06-30 23:02:08 +0100

    Update parsers and package.json

---
+++ src/full.js
@@ -0,0 +1,224 @@
+// =============================================================================
+// Full Interactive Parser for Mathigon Textbooks
+// (c) Mathigon
+// =============================================================================
+
+
+const path = require('path');
+const yaml = require('yamljs');
+const grunt = require('grunt');
+const jade = require('jade');
+const marked = require('marked');
+const ascii2mathml = require("ascii2mathml");
+
+
+const intro = '<div class="chapter-intro"><span class="user">Remember to <x-target to=".nav-link.dropdown-title">login</x-target> to save your progress and get personalised content.</span> <span class="size">This textbook works best on larger devices like tablets or laptops.</span></div>';
+
+let bios = [];
+
+// -----------------------------------------------------------------------------
+
+function repeat(text, n) {
+  let result = text;
+  for (let i=1; i<n; ++i) result += text;
+  return result;
+}
+
+function decodeHTML(html) {
+  let replacements = [['amp', '&'], ['quot', '"'], ['apos', '\''], ['lt', '<'], ['gt', '>']];
+  for (let r of replacements) {
+    html = html.replace(new RegExp('&' + r[0] + ';', 'g'), r[1]);
+  }
+  return html;
+}
+
+function parseJade(text, path) {
+  // Jade code is indented by two spaces, which we need to remove.
+  text = text.slice(2).replace(/\n\s\s/g, '\n')
+    .replace(/(images|svg)\//g, (x, f) => `/resources/${path}/${f}/` );
+  return jade.render(text);
+}
+
+function parseSection(text) {
+  let tags = text.match('\-*(\{(.+)\})?')[2] || '';
+  return jade.render('section' + tags).replace('</section>', '');
+}
+
+// -----------------------------------------------------------------------------
+
+const renderer = new marked.Renderer();
+
+renderer.link = function(href, title, text) {
+  if (href.startsWith('gloss:')) return `<x-gloss xid="${href.slice(6)}">${text}</x-gloss>`;
+  if (href.startsWith('bio:')) {
+    let id = href.slice(4);
+    if (!bios.includes(id)) bios.push(id);
+    return `<x-bio xid="${id}">${text}</x-bio>`;
+  }
+  return `<a href="${href}" target="_blank">${text}</a>`;
+};
+
+renderer.code = renderer.codespan = function(code) {
+  let maths = ascii2mathml(code, {bare: true});
+  maths = maths.replace(/<mo>-<\/mo>/, '<mo>–</mo>');
+  return '<span class="math">' + maths + '</span>';
+  // .replace(/<mrow>\s*<mo>\(<\/mo>/g, '<mfenced>')
+  // .replace(/<mo>\)<\/mo>\s*<\/mrow>/g, '</mfenced>');
+  // math = minify(math, { collapseWhitespace: true });
+};
+
+function parseMarkdown(text, path) {
+  text = text
+    .replace(/\[\[([^\]]+)\]\]/g, function(x, body) {
+      if (body.split('|').length > 1) return `<x-blank choices="${body}"></x-blank>`;
+      return `<x-blank input="${body}"></x-blank>`;
+    })
+    .replace(/\[([\w\s\-]+)\]\(->([^\)]+)\)/g, function(x, text, target) {
+      return `<x-target to="${target}">${text}</x-target>`
+    })
+    .replace(/\.subsection\(([\w\s\-]+)\)/g, function(x, text) {
+      return `.subsection(data-needs="${text}")`;
+    })
+    .replace(/\.gloss\(([\w\s\-]+)\)/g, function(x, text) {
+      return `(data-gloss="${text}")`;
+    })
+    .replace(/n't/g, 'n’t')
+    .replace(/t's/g, 't’s');
+
+  // Add classes to entire blocks
+  let rows = text.split('\n');
+  let blockTag = null;
+  if (rows[0].match(/^\{([^\}]+)\}$/)) {
+    blockTag = rows[0].slice(1, rows[0].length - 1).trim();
+    text = rows.slice(1).join('\n');
+  }
+
+  // Support tables without headers
+  if (text.startsWith('| ')) {
+    let columns = Math.max(...text.split('\n').map(row => row.split(' | ').length));
+    let header = `${repeat('| ', columns)}|\n${repeat('| --- ', columns)}|\n`;
+    text = header + text;
+  }
+
+  // TODO Replace emojis :smile:
+  // TODO jade imports (e.g. SVGs)
+
+  let result = marked(text, {renderer/*, sanitize: true, smartypants: true*/})
+    .replace(/<([\w\-]+)>\{([^\}]+)\}\s*/g, function(x, tag, options) {
+      // expand <p>{.x}</p> to <p class="x"></p>
+      return jade.render(tag + decodeHTML(options)).replace(/<\/.+>/, '');
+    })
+    .replace(/\$\{([^\}]+)\}\{([^\}]+)\}/g, function(x, body, slider) {
+      return `<x-var bind="${slider}">\$\{${body}\}</x-var>`;
+    })
+    .replace(/\$\{([^\}]+)\}(?!\<\/x\-var\>)/g, function(x, body) {
+      return `<span class="var">\$\{${body}\}</span>`;
+    })
+    .replace(/\[\{([^\}]+)\}\s*([^\]]*)\]/g, function(x, options, body) {
+      // expand [{.x}] to <span class="x"></span>
+      let tag = '.#('.includes(options[0]) ? 'span' : '';
+      return jade.render(decodeHTML(tag + options + ' ' + body));
+    });
+
+  if (blockTag) {
+    result = result.replace(/^<([\w\-]+)>/g, function(x, tag) {
+      return jade.render(tag + decodeHTML(blockTag)).replace(/<\/.+>/, '');
+    });
+  }
+
+  return result;
+}
+
+// -----------------------------------------------------------------------------
+
+function parsePart(part, path) {
+  return (part.startsWith('  ') ? parseJade : parseMarkdown)(part, path);
+}
+
+function generate(path, text, allBios) {
+  // Filter out all comment lines:
+  text = text.replace(/\n\s*\/\/[^\n]*\n/g, '\n');
+
+  let result = '';
+  let part = '';
+
+  bios = [];
+  let data = {};
+
+  for (let p of text.split(/\n\s*\n/)) {
+
+    if (p.startsWith('---')) {
+      // Section dividers
+      let showIntro = result ? '' : intro;
+      if (part) result += parsePart(part, path);
+      part = '';
+      if (result) result += '</section>';
+      result += parseSection(p) + showIntro;
+
+    } else if (!result) {
+      // Into content that is not rendered
+      if (p.startsWith('#')) {
+        data.title = p.slice(2);
+      } else {
+        Object.assign(data, yaml.parse(p));
+      }
+
+    } else {
+      // Actual content
+      if (p.startsWith('  ')) {
+        if (part.startsWith('  ')) {
+          part += '\n' + p;
+        } else {
+          result += parseMarkdown(part, path);
+          part = p;
+        }
+      } else {
+        result += parsePart(part, path);
+        part = p;
+      }
+    }
+  }
+
+  result += parsePart(part, path) + '</section>';
+  result = result.replace(/[\n\s]+/g, ' ');  // minify html
+
+  let fullBios = {};
+  for (let b of bios) fullBios[b] = allBios[b];
+
+  return {html: result, bios: JSON.stringify(fullBios), data: JSON.stringify(data)};
+}
+
+
+module.exports = function(src, dest, root) {
+  // TODO convert allBios to YAML and parse here
+  const allBios = require('../../textbooks/shared/bios.json');
+
+  src = path.join(__dirname, '../', src[0]);
+  let id = src.split('/')[src.split('/').length - 1];
+
+  // DEPRECATED Old chapters that have JADE rather than Markdown.
+  if (!grunt.file.exists(src, 'content.md')) {
+    let content = grunt.file.read(src + '/content.jade');
+    let html = jade.render(content, {filename: src + '/content.jade'});
+
+    let bios = {};
+    for (let b of content.match(/bio\(xid=['"]\w*['"]/g).map(b => b.slice(9, -1))) {
+      bios[b] = allBios[b];
+    }
+
+    return Promise.all([
+      grunt.file.write(dest + '/content.html', html),
+      grunt.file.write(dest + '/bios.json', JSON.stringify(bios))
+    ]);
+  }
+
+  // TODO convert glossary to YAML and parse here
+  let content = grunt.file.read(src + '/content.md');
+  let {html, bios, data} = generate(id, content, allBios);
+
+  return Promise.all([
+    grunt.file.write(dest + '/content.html', html),
+    grunt.file.write(dest + '/bios.json', bios),
+    grunt.file.write(dest + '/data.json', data)
+  ]);
+};
---
+++ src/text.js
@@ -0,0 +1,47 @@
+// =============================================================================
+// Plain Text Parser for Mathigon Textbooks
+// (c) Mathigon
+// =============================================================================
+
+
+const path = require('path');
+const grunt = require('grunt');
+const marked = require('marked');
+
+
+const renderer = new marked.Renderer();
+renderer.link = function(href, title, text) { return text; };
+renderer.heading = function(text) { return `\n\n${text}\n\n`; };
+renderer.listitem = function(text) { return `* ${text}\n`; };
+renderer.hr = function() { return `\n\n`; };
+renderer.br = function() { return `\n`; };
+renderer.code = renderer.paragraph = renderer.list = (text => `${text}\n\n`);
+renderer.codespan = renderer.strong = renderer.em = renderer.del = (code => code);
+renderer.image = ((href, title, text) => '');
+
+function generate(text) {
+  let result = text
+    .replace(/\n\s*\/\/[^\n]*\n/g, '\n')  // Remove Comments.
+    .replace(/\n\|\s[^\n]*\n/g, '\n')  // Remove Tables.
+    .split(/\n\s*\n/)
+    .splice(1, 1)  // Remove the second paragraph, which is data.
+    .filter(p => !p.startsWith('---'))  // Remove section dividers.
+    .filter(p => !p.startsWith('  '))  // Remove Jade code.
+    .join('\n\n')
+    .replace(/\[\[([^\]]+)\]\]/g, (x, body) => body.split('|')[0])  // Blanks.
+    .replace(/\[([\w\s\-]+)\]\(->([^\)]+)\)/g, (x, text) => `${text}`)
+    .replace(/\n\{([^\}]+)\}\n/g, '\n');  // Paragraph tags.
+
+  return marked(result, {renderer})
+    .replace(/\{([^\}]+)\}\s*/g, () => '')
+    .replace(/\$\{([^\}]+)\}\{([^\}]+)\}/g, (x, body) => body)
+    .replace(/\$\{([^\}]+)\}(?!\<\/x\-var\>)/g, (x, body) => body)
+    .replace(/\[\{([^\}]+)\}\s*([^\]]*)\]/g, (x, options, body) => body)
+    .trim();
+}
+
+module.exports = function(src, dest, root) {
+  if (!grunt.file.exists(src, 'content.md')) return;
+  const text = generate(grunt.file.read(src + '/content.md'));
+  return grunt.file.write(dest + '/content.txt', text);
+};
