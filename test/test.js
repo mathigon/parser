fe6dcfb9754eff96e75145b43cac7d0fc81e77f8
Philipp Legner <philipp@mathigon.org>
2018-04-21 11:05:36 +0100

    Fix dependencies

--- test/test.js
+++ test/test.js
@@ -15,6 +15,4 @@ const dir = path.join(process.cwd(), 'test');
 const source = fs.readFileSync(dir + '/input.md', 'utf8');
 const output = parseFull('test', source, dir);
 
-console.log('Biographies:', output.bios);
-console.log('Data:', output.data);
 fs.writeFileSync(dir + '/output.html', output.html);
