84814b524bd4000cb286fa5bb93f0293f408f9ca
Philipp Legner <philipp@mathigon.org>
2019-02-27 14:44:18 +0100

    Improved internationalisation fallbacks

--- index.js
+++ index.js
@@ -15,28 +15,39 @@ const {parseFull, parseSimple} = require('./src/full');
 
 // -----------------------------------------------------------------------------
 
-function loadFile(src, name, locale) {
-  const localFile = name.replace('.', `_${locale}.`);
-  const localPath = path.join(src, 'translations', localFile);
-  if (fs.existsSync(localPath)) return fs.readFileSync(localPath, 'utf8');
-
-  const defaultPath = path.join(src, name);
-  if (fs.existsSync(defaultPath)) return fs.readFileSync(defaultPath, 'utf8');
-
-  return '';
+function loadFile(root, name, locale) {
+  if (locale !== 'en') name = 'translations/' + name.replace('.', `_${locale}.`);
+  const file = path.join(root, name);
+  return fs.existsSync(file) ? fs.readFileSync(file, 'utf8') : '';
 }
 
-function generate(grunt, src, dest, id, allBios, allGloss, sharedHints, locale, cache) {
-  let content = loadFile(src, 'content.md', locale);
+function loadYAML(path, name, textField = null, locale = 'en', fallback = {}) {
+  const text = loadFile(path, name, locale);
+  const data = text ? yaml.parse(text) || {} : {};
 
-  // Only parse files that changed.
-  if (cache) {
-    const hash = crypto.createHash('md5').update(content).digest('hex');
-    if (cache[id + '-' + locale] === hash) return;
-    cache[id + '-' + locale] = hash;
-    console.log(`>> Parsing ${id} / ${locale}`);
+  for (let d of Object.keys(data)) {
+    if (textField) {
+      // Used for bios and glossary.
+      data[d][textField] = parseSimple(data[d][textField]);
+    } else if (Array.isArray(data[d])) {
+      // Used for hint arrays
+      data[d] = data[d].map(x => parseSimple(x));
+    } else {
+      // Used for individual hints.
+      data[d] = parseSimple(data[d]);
+    }
   }
 
+  for (let d of Object.keys(fallback)) {
+    if (!data[d]) data[d] = fallback[d];
+  }
+
+  return data;
+}
+
+// -----------------------------------------------------------------------------
+
+function generate(grunt, content, src, dest, id, allBios, allGloss, sharedHints, locale) {
   let {bios, gloss, data, stepsHTML, sectionsHTML} = parseFull(id, content, src);
   grunt.file.write(dest + '/data.json', JSON.stringify(data));
 
@@ -54,11 +65,9 @@ function generate(grunt, src, dest, id, allBios, allGloss, sharedHints, locale, 
   }
   grunt.file.write(dest + '/glossary.json', JSON.stringify(glossObj));
 
-  const hintsObj = {};
-  const hints = yaml.parse(loadFile(src, 'hints.yaml', locale) || '{}');
-  for (let h of Object.keys(hints)) hintsObj[h] = parseSimple(hints[h]);
-  Object.assign(hintsObj, sharedHints);
-  grunt.file.write(dest + '/hints.json', JSON.stringify(hintsObj));
+  // TODO Fall back to the English hints if missing!
+  const hints = loadYAML(src, 'hints.yaml', null, locale, sharedHints);
+  grunt.file.write(dest + '/hints.json', JSON.stringify(hints));
 
   for (let s of Object.keys(stepsHTML)) {
     grunt.file.write(dest + `/steps/${s}.html`, stepsHTML[s]);
@@ -74,33 +83,38 @@ function generate(grunt, src, dest, id, allBios, allGloss, sharedHints, locale, 
 module.exports = function(grunt) {
   grunt.registerMultiTask('textbooks', 'Mathigon Markdown parser.', function() {
     const options = this.options({root: 'content', languages: ['en'], cache: false});
+
     const root = path.join(process.cwd(), options.root);
+    const shared = root + '/shared';
 
     const cacheFile = path.join(process.cwd(), this.files[0].dest, '../cache.json');
-    const cache = options.cache ? (grunt.file.exists(cacheFile) ? grunt.file.readJSON(cacheFile) : {}) : null;
+    const cache = grunt.file.exists(cacheFile) ? grunt.file.readJSON(cacheFile) : {};
+
+    const bios = loadYAML(shared, 'bios.yaml', 'bio');
+    const gloss = loadYAML(shared, 'glossary.yaml', 'text');
+    const hints = loadYAML(shared, 'hints.yaml');
 
     for (let locale of options.languages) {
-
-      const bios = yaml.parse(loadFile(root + '/shared', 'bios.yaml', locale));
-      for (let b of Object.keys(bios)) bios[b].bio = parseSimple(bios[b].bio);
-
-      const gloss = yaml.parse(loadFile(root + '/shared', 'glossary.yaml', locale));
-      for (let g of Object.keys(gloss)) gloss[g].text = parseSimple(gloss[g].text);
-
-      const hints = yaml.parse(loadFile(root + '/shared', 'hints.yaml', locale));
-      for (let h of Object.keys(hints)) {
-        if (Array.isArray(hints[h])) {
-          hints[h] = hints[h].map(h => parseSimple(h));
-        } else {
-          hints[h] = parseSimple(hints[h]);
-        }
-      }
+      const localBios = loadYAML(shared, 'bios.yaml', 'bio', locale, bios);
+      const localGloss = loadYAML(shared, 'glossary.yaml', 'text', locale, gloss);
+      const localHints = loadYAML(shared, 'hints.yaml', null, locale, hints);
 
       for (let file of this.files) {
         const id = file.src[0].split('/')[file.src[0].split('/').length - 1];
         const src = path.join(process.cwd(), file.src[0]);
         const dest = path.join(process.cwd(), file.dest, locale);
-        generate(grunt, src, dest, id, bios, gloss, hints, locale, cache)
+
+        const content = loadFile(src, 'content.md', locale);
+        if (!content) continue;
+
+        if (options.cache) {
+          const hash = crypto.createHash('md5').update(content).digest('hex');
+          if (cache[id + '-' + locale] === hash) continue;
+          cache[id + '-' + locale] = hash;
+          console.log(`>> Parsing ${id} / ${locale}`);
+        }
+
+        generate(grunt, content, src, dest, id, localBios, localGloss, localHints, locale)
       }
     }
 
