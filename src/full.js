db9f8e1547952cc547b5ea7ac968e3194727edfc
Philipp Legner <philipp@mathigon.org>
2019-02-28 16:28:59 +0100

    Remove imgSequence component

--- src/full.js
+++ src/full.js
@@ -295,7 +295,7 @@ module.exports.parseFull = function(id, content, path) {
   const sections = [];
   let goals = 0;
 
-  const autoGoals = 'x-blank, x-blank-input, x-equation, x-var, x-slider, x-img-sequence, .next-step, x-sortable, x-gameplay, x-slideshow .slide, x-slideshow .legend, x-picker .item:not([data-error])';
+  const autoGoals = 'x-blank, x-blank-input, x-equation, x-var, x-slider, .next-step, x-sortable, x-gameplay, x-slideshow .slide, x-slideshow .legend, x-picker .item:not([data-error])';
 
   const $steps = doc.body.querySelectorAll('x-step');
   for (let i = 0; i < $steps.length; ++i) {
