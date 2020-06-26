67752d97cc6290f620e140d900b6c7246d50126d
Philipp Legner <philipp@mathigon.org>
2020-06-26 15:03:54 +0200

    Better error handling and logging

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ const {getAudioTimings, writeAudioTimings, extractText, textHash} = require('./s
 const YAML_CACHE = new Map();
 
 
+function warning(...msg) {
+  console.warn('\x1b[31m', ...msg, '\x1b[0m');
+}
+
 function loadFile(root, name, locale) {
   if (locale !== 'en') name = 'translations/' + name.replace('.', `_${locale}.`);
   const file = path.join(root, name);
@@ -77,33 +81,38 @@ function createFile(dest, name, content) {
   });
 }
 
+async function combineData(name, parsed, dest, courseId, textField, locale) {
+  const missing = [];
+  const data = await loadYAML(dest + 'shared', name + '.yaml', textField, locale);
+  const obj = {};
+
+  for (let id of parsed) {
+    if (id in data) {
+      obj[id] = data[id];
+    } else {
+      missing.push(id);
+    }
+  }
+
+  if (locale === 'en' && missing.length) {
+    warning(`  Missing ${name}: ${missing.join(', ')}`);
+  }
+
+  return createFile(dest, `${courseId}/${name}_${locale}.json`, JSON.stringify(obj));
+}
+
 async function generate(content, base, id, locale) {
-  const dest = path.join(base, '../');
-  const shared = path.join(base, '../shared');
-
   const {bios, gloss, data} = await parse(id, content, base, locale);
+  const dest = path.join(base, '../');
 
-  const biosData = await loadYAML(shared, 'bios.yaml', 'bio', locale);
-  const biosObj = {};
-  for (let b of bios) {
-    if (!(b in biosData)) console.warn('Missing bio: ' + b);
-    biosObj[b] = biosData[b];
-  }
-  const biosFile = createFile(dest, `${id}/bios_${locale}.json`, JSON.stringify(biosObj));
-
-  const glossData = await loadYAML(shared, 'glossary.yaml', 'text', locale);
-  const glossObj = {};
-  for (let g of gloss) {
-    if (!(g in glossData)) console.warn('Missing glossary: ' + g);
-    glossObj[g] = glossData[g];
-  }
-  const glossFile = createFile(dest, `${id}/glossary_${locale}.json`, JSON.stringify(glossObj));
+  const biosFile = await combineData('bios', bios, dest, id, 'bio', locale);
+  const glossFile = await combineData('glossary', gloss, dest, id, 'text', locale);
 
   const quizObj = await loadYAML(base, 'quiz.yaml', 'text,choices,hints', locale);
   const quizFile = createFile(dest, `${id}/quiz_${locale}.json`, JSON.stringify(quizObj));
 
   const courseHints = await loadYAML(base, 'hints.yaml', '*', locale);
-  const globalHints = await loadYAML(shared, 'hints.yaml', '*', locale);
+  const globalHints = await loadYAML(dest + 'shared', 'hints.yaml', '*', locale);
   const hintsObj = Object.assign({}, globalHints, courseHints);
   const hintsFile = createFile(dest, `${id}/hints_${locale}.json`, JSON.stringify(hintsObj));
 
@@ -121,6 +130,8 @@ module.exports.gulp = (languages = ['en'], cacheFile = '') => {
         JSON.parse(fs.readFileSync(cacheFile, 'utf8')) : {};
 
     const promises = [];
+    const locales = [];
+
     for (let locale of languages) {
       const content = loadFile(file.path, 'content.md', locale);
       if (!content) continue;
@@ -129,12 +140,13 @@ module.exports.gulp = (languages = ['en'], cacheFile = '') => {
       if (cacheData[file.basename + '-' + locale] === hash) continue;
       cacheData[id + '-' + locale] = hash;
 
-      console.log(`>> Parsing ${id} / ${locale}`);
+      locales.push(locale);
       promises.push(generate(content, file.path, id, locale).catch((error) => {
-        console.error(`Failed to parse ${id} / ${locale}:`, error.message);
+        warning(`Failed to parse ${id} [${locale}]:`, error.message);
       }));
     }
 
+    console.log(`>> Parsing ${id} [${locales.join(', ')}]`);
     if (cacheFile) fs.writeFileSync(cacheFile, JSON.stringify(cacheData));
 
     const fileSets = await Promise.all(promises);
